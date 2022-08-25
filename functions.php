ceb96858e3a73f07812d2613b5a5be0f371e814b
robertocannella <robertocannella@gmail.com>
2022-08-25 16:11:55 -0400

    Implement wp_enqueue() functions

--- functions.php
+++ functions.php
@@ -6,3 +6,21 @@
  * @package Aquila
  * 
  */
+
+// DEBUG TOOL
+//echo '<pre>';
+//print_r(get_template_directory_uri());
+//wp_die();
+
+// Load our scripts
+function aquila_enqueue_scripts(){
+    /* REGISTER STYLES AND SCRIPTS */
+   //wp_enqueue_style('main-css',get_template_directory_uri() . '/main.css', ['stylesheet']);
+    wp_register_style('style-css', get_stylesheet_uri(),[],filemtime(get_template_directory() . '/style.css'),'all');
+    wp_register_script('main-js', get_template_directory_uri() . '/assets/main.js',[],filemtime( get_template_directory() . '/assets/main.js'), true);
+
+    /* ENQUEUE STYLES AND SCRIPTS */
+    wp_enqueue_style('style-css');
+    wp_enqueue_script('main-js');
+}
+add_action('wp_enqueue_scripts', 'aquila_enqueue_scripts');
--- header.php
+++ header.php
@@ -16,5 +16,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <?php wp_head();?>
  </head>
- <body>
+ <body <?php body_class('custom-class');?>>
+ <?php wp_body_open();?>
 <header></header>
